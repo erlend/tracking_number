a02174ec61199a0fc82e46007854ae32bea71607
Erlend Finvåg <erlend@users.noreply.github.com>
2022-10-12 16:00:16 +0200

    Fix failing tests

--- lib/tracking_number/info.rb
+++ lib/tracking_number/info.rb
@@ -1,26 +1,12 @@
 module TrackingNumber
-  class Info
+  class Info < OpenStruct
     def initialize(info_hash = {})
-      info_hash.keys.each do |key|
-        self.instance_variable_set("@#{key}", info_hash[key])
-        self.class_eval { attr_accessor key }
-      end
-
-      if info_hash.keys.size == 1
-        @default = info_hash[info_hash.keys.first]
-      end
-    end
-
-    def method_missing(*args)
-      nil
+      @default = info_hash.values.first if info_hash.size == 1
+      super
     end
 
     def to_s
       @default || @name
-    end
-
-    def to_json
-
     end
   end
 end
